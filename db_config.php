1d61da4c83344564142a6b9e7f7429ca1b057814
Pt. Prashant tripathi <26687933+PtPrashantTripathi@users.noreply.github.com>
2021-05-07 11:13:17 +0530

    Add files via upload

--- db_config.php
+++ db_config.php
@@ -1,14 +1,27 @@
 <?php
 date_default_timezone_set("asia/calcutta");
 // Mysql database settings
-$user		= "root";
-$password	= "usbw";
-$database	= "milk";
-$host		= "localhost";
+$user = "root";
+$password = "usbw";
+$database = "milk";
+$host = "localhost";
 
-mysql_connect($host,$user,$password);
-mysql_select_db($database) or die( "Unable to select database");
+mysql_connect($host, $user, $password);
+mysql_select_db($database) or die("Unable to select database");
 
-$mts=['January','Febuary','March','April','May','June','July','August','September','October','November','December'];
+$mts = [
+    'January',
+    'Febuary',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
 
 ?>
